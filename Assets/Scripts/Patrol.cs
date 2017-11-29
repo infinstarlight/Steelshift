509147345a04b766f0b20823490ef6c53ccbdcc9
Brandon Jones <bcjones1@fullsail.edu>
2017-11-29 17:42:58 -0500

    Completed basic NavMesh generation and added AI that can move from Transform points. TODO: Add movement delay, perception and functionallity to detect player

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour {
+
+    public Transform[] points;
+    public float MovementDelay;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+
+            //TODO: Introduce delay timer for AI movement
+
+            GoToNextPoint();
+
+    }
+
+    void GoToNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+}
