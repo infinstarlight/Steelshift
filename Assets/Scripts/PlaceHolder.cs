9ecca77cb7b9056fbe89a87ff5add1a5fff0203a
Hollingsworth <jholl98@gmail.com>
2017-12-02 15:55:01 -0500

    transition scripts are done. updated transform so ethan spawns on top of scene 1 position. prefabs are updated on character and scene manager.

---
+++ Assets/Scripts/PlaceHolder.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaceHolder : MonoBehaviour {
+
+    public GameObject Player;
+    public string placeScene;
+    public  bool Ethancheck;
+    public Transform spawner1;
+    public static bool Ethan2;
+    public GameObject secondPlayer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        DontDestroyOnLoad(transform.gameObject);
+        Scene PlaceScene = SceneManager.GetActiveScene();
+        string Scenename = PlaceScene.name; 
+        if(Scenename == placeScene)
+        {
+            Player.SetActive(true);
+        }
+        if(Ethancheck == true)
+        {
+            Scene currentscene = SceneManager.GetActiveScene();
+            string sceneName = currentscene.name;
+            if (Ethan2 == false && sceneName == "Debug scene 2")
+            {
+                Instantiate(secondPlayer, spawner1.position, spawner1.rotation);
+                Ethan2 = true;
+            }
+            if(sceneName == "Debug scene 1")
+            {
+                Ethan2 = false;
+            }
+        }
+
+	}
+}
---
+++ Assets/Scripts/TransitionSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionSystem : MonoBehaviour {
+
+    private Transition[] points;
+    public Transform playerPos;
+    private Vector3 endPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        points = GameObject.FindObjectsOfType<Transition>();
+        foreach (Transition tp in points)
+        {
+            if (tp.isHere == true)
+            {
+                tp.exitPoint.isExit = true;
+                endPos = tp.exitPoint.transform.position;
+                tp.isHere = false;
+            }
+        }
+	}
+}
