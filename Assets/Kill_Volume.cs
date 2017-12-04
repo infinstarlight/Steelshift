592fd99ca14758cead1e006347fdcdfa9142c470
Brandon Jones <bcjones1@fullsail.edu>
2017-12-04 17:02:32 -0500

    Creating GameOver Scene

---
+++ Assets/Kill_Volume.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill_Volume : MonoBehaviour {
+
+    private GameObject Player;
+    public Player_Stats PStats;
+
+	// Use this for initialization
+	void Start () {
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PStats.Health = 0;
+        }
+    }
+}
