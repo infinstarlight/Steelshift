302f593689ca3dc0c4099f2c4dfc3212bdf4463e
Brandon Jones <bcjones1@fullsail.edu>
2017-12-07 16:14:41 -0500

    Made another level segment, just need to fix scripting

---
+++ Assets/BJ_EnterRoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BJ_EnterRoom : MonoBehaviour {
+
+    public GameObject Room;
+    public Animator RoomAnim;
+
+	// Use this for initialization
+	void Start () {
+
+        RoomAnim = Room.GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (RoomAnim.GetBool("Entered?") == false)
+            {
+                RoomAnim.SetBool("Entered?", true);
+            }
+        }
+    }
+}
